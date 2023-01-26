2b65d2719e4747ff287c5d2b6dbd809568881a9d
Fazal Wahid <you@example.com>
2023-01-26 22:05:42 +0400

    Template upgraded to dotnet 6

---
+++ CoreTemplate.App.Api6/Controllers/AccountNatureController.cs
@@ -0,0 +1,59 @@
+﻿namespace CoreTemplate.App.Api.Controllers.v1;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountNatureController : ControllerBase
+{
+private readonly IAccountService _accNatureService;
+public AccountNatureController(IAccountService accService) {
+    _accNatureService = accService;
+}
+// GET: api/<ChartOfAccount>
+[HttpGet]
+public async Task<IActionResult> Get()
+{
+    return Ok(await _accNatureService.GetAllAccountNaturesAsync());
+}
+
+// GET api/<ChartOfAccount>/5
+[HttpGet("{id}")]
+public async Task<IActionResult> Get(int id)
+{
+    return Ok(await _accNatureService.GetAccountNatureAsync(id));
+}
+
+// POST api/<ChartOfAccount>
+[HttpPost]
+public async Task<IActionResult> Post([FromBody] AccountNatureDTO value)
+{
+    if (ModelState.IsValid)
+    {
+        var acc = await _accNatureService.CreateAccountNatureAsync(value);
+        // Return 201
+        return new ObjectResult(acc) { StatusCode = StatusCodes.Status201Created };
+    }
+    return BadRequest(ModelState);            
+}
+
+// PUT api/<ChartOfAccount>/5
+[HttpPatch("{id}")]
+public async Task<IActionResult> Patch(int id, [FromBody] AccountNatureDTO value)
+{
+    if (ModelState.IsValid)
+    {
+        // Return 201
+        value.AccountNatureId = id;
+        return Ok(await _accNatureService.UpdateAccountNatureAsync(value));
+    }
+    return BadRequest(ModelState);
+}
+
+// DELETE api/<ChartOfAccount>/5
+[HttpDelete("{id}")]
+public async Task<IActionResult> Delete(int id)
+{
+    _ = await _accNatureService.DeleteAccountNatureAsync(id);
+    return NoContent();
+}
+}
+
---
+++ CoreTemplate.App.Api6/Extensions/Database.cs
@@ -0,0 +1,22 @@
+﻿using CoreTemplate.App.Db;
+using CoreTemplate.App.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreTemplate.App.Api.Extensions
+{
+    public static class UseDatabase
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            //configure entity framecore
+            services.AddDbContext<IdentityContext>(
+            options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<AppDbContext>(
+            options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));            
+            return services;
+        }
+    }
+}
